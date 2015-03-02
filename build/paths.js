6bc87bfdd648e946a859ff076b0192e7811fb7f2
Dmytro Yarmak <dmytroyarmak@gmail.com>
2015-03-02 19:38:56 +0200

    Configure gulp with clean task

---
+++ build/paths.js
@@ -0,0 +1,9 @@
+var appRoot = 'src/';
+
+module.exports = {
+  root: appRoot,
+  source: appRoot + 'app/**/*.js',
+  html: appRoot + 'app/**/*.html',
+  style: appRoot + 'styles/**/*.css',
+  output: 'dist/'
+};
