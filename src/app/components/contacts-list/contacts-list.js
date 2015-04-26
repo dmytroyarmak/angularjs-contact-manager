94f5c75ab100ded22731e47f51afbb6154933dd0
Dmytro Yarmak <dmytroyarmak@gmail.com>
2015-04-26 09:17:11 +0300

    Restructure comoenents

--- src/app/components/contacts-list/contacts-list.js
+++ src/app/components/contacts-list/contacts-list.js
@@ -1,5 +1,38 @@
 'use strict';
 
-angular.module('contactManager.contactsList', [
-  'contactManager.services.contactManagerApi'
-]);
+angular
+  .module('contactManager.contactsList', [
+    'contactManager.services.contactManagerApi'
+  ])
+  .controller('ContactsListController', ContactsListController);
+
+/* @ngInject */
+function ContactsListController(contactManagerApi) {
+  var contactsList = this;
+  contactsList.deleteContact = deleteContact;
+  contactsList.activate = activate;
+  contactsList.contacts = [];
+
+  //////////
+
+  function activate() {
+    return _getAllContacts();
+  }
+
+  function deleteContact(contact) {
+    var indexOfContact = contactsList.contacts.indexOf(contact);
+    contactsList.contacts.splice(indexOfContact, 1);
+
+    contactManagerApi.deleteContact(contact.id);
+  }
+
+  function _getAllContacts() {
+    contactManagerApi
+      .getAllContacts()
+      .then(_onGetAllContactsSuccess);
+  }
+
+  function _onGetAllContactsSuccess(contacts) {
+    contactsList.contacts = contacts;
+  }
+}
